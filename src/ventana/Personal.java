3e1f2eedb6d453480d90c089db56a20a906dd753
JavierLuisG <javierandresluisgonzalez@gmail.com>
2024-02-04 17:38:31 -0500

    Primer commit, diseño del JFrame para el CRUD

---
+++ src/ventana/Personal.java
@@ -0,0 +1,221 @@
+package ventana;
+
+public class Personal extends javax.swing.JFrame {
+
+    public Personal() {
+        initComponents();
+        // Permite localizar en el centro de la pantalla
+        setLocationRelativeTo(null);
+        // No permite modificar el tamaño
+        setResizable(false);
+        // No necesitamos la visibilidad de la cajaId
+        cajaId.setVisible(false);
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        btnBuscar = new javax.swing.JButton();
+        jLabel8 = new javax.swing.JLabel();
+        cajaBuscar = new javax.swing.JTextField();
+        cajaId = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        cajaIdentificacion = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        cajaNombre = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        cajaEmail = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        cajaDireccion = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        cajaCelular = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        cajaIngreso = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        comboGenero = new javax.swing.JComboBox<>();
+        btnRegistrar = new javax.swing.JButton();
+        btnActualizar = new javax.swing.JButton();
+        btnBorrar = new javax.swing.JButton();
+        btnLimpiar = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnBuscar.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        btnBuscar.setText("Buscar");
+        btnBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBuscarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnBuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 30, 100, 30));
+
+        jLabel8.setForeground(new java.awt.Color(153, 153, 153));
+        jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel8.setText("Ingrese No. Identificación");
+        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 15, 146, -1));
+
+        cajaBuscar.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        jPanel1.add(cajaBuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 33, 146, 24));
+
+        cajaId.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        jPanel1.add(cajaId, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 70, 78, -1));
+
+        jLabel1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel1.setText("Número Identificación:");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 100, -1, 24));
+
+        cajaIdentificacion.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        jPanel1.add(cajaIdentificacion, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 100, 230, 24));
+
+        jLabel2.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel2.setText("Nombre y apellidos:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 136, -1, 24));
+
+        cajaNombre.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        jPanel1.add(cajaNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 136, 230, 24));
+
+        jLabel3.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel3.setText("Correo electrónico:");
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 172, -1, 24));
+
+        cajaEmail.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        jPanel1.add(cajaEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 172, 230, 24));
+
+        jLabel4.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel4.setText("Dirección:");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 208, -1, 24));
+
+        cajaDireccion.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        jPanel1.add(cajaDireccion, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 208, 230, 24));
+
+        jLabel5.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("Celular:");
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 240, 160, 24));
+
+        cajaCelular.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        jPanel1.add(cajaCelular, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 270, 158, -1));
+
+        jLabel6.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setText("Fecha ingreso:");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 240, 170, 24));
+
+        cajaIngreso.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        jPanel1.add(cajaIngreso, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 270, 172, -1));
+
+        jLabel7.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel7.setText("Genero:");
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 310, 68, 24));
+
+        comboGenero.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        comboGenero.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Masculino", "Femenino" }));
+        jPanel1.add(comboGenero, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 310, 180, -1));
+
+        btnRegistrar.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        btnRegistrar.setText("Registrar");
+        btnRegistrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRegistrarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnRegistrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 390, 100, 30));
+
+        btnActualizar.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        btnActualizar.setText("Actualizar");
+        btnActualizar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnActualizarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnActualizar, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 390, 100, 30));
+
+        btnBorrar.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        btnBorrar.setText("Borrar");
+        btnBorrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnBorrarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnBorrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 390, 100, 30));
+
+        btnLimpiar.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        btnLimpiar.setText("Limpiar");
+        btnLimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnLimpiarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnLimpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 390, 100, 30));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 450, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 442, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLimpiarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnLimpiarActionPerformed
+
+    private void btnBorrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBorrarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnBorrarActionPerformed
+
+    private void btnActualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnActualizarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnActualizarActionPerformed
+
+    private void btnRegistrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRegistrarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnRegistrarActionPerformed
+
+    private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
+
+    }//GEN-LAST:event_btnBuscarActionPerformed
+
+    public static void main(String args[]) {
+
+        java.awt.EventQueue.invokeLater(() -> {
+            new Personal().setVisible(true);
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnActualizar;
+    private javax.swing.JButton btnBorrar;
+    private javax.swing.JButton btnBuscar;
+    private javax.swing.JButton btnLimpiar;
+    private javax.swing.JButton btnRegistrar;
+    private javax.swing.JTextField cajaBuscar;
+    private javax.swing.JTextField cajaCelular;
+    private javax.swing.JTextField cajaDireccion;
+    private javax.swing.JTextField cajaEmail;
+    private javax.swing.JTextField cajaId;
+    private javax.swing.JTextField cajaIdentificacion;
+    private javax.swing.JTextField cajaIngreso;
+    private javax.swing.JTextField cajaNombre;
+    private javax.swing.JComboBox<String> comboGenero;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+}
